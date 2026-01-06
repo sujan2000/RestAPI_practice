314709b3074ce7d07d9a7c10cba1af4c1e5123fc
sujan2000 <meena.sujan06@gmail.com>
2026-01-06 22:11:27 +0530

    make case insensitive and make response availble to public to allow everyone access

--- utils/getDataByQueryParams.js
+++ utils/getDataByQueryParams.js
@@ -5,20 +5,16 @@ export function getDataByQueryParams(data, queryObj) {
     const { continent, country, is_open_to_public } = queryObj
 
     if (continent) {
-        data = data.filter(dest =>
-            dest.continent.toLowerCase() === continent.toLowerCase()
-        )
+        data = data.filter(dest => dest.continent.toLowerCase() === continent.toLowerCase())
     }
 
     if (country) {
-        data = data.filter(dest =>
-            dest.country.toLowerCase() === country.toLowerCase()
-        )
+        data = data.filter(dest => dest.country.toLowerCase() === country.toLowerCase())
     }
     if (is_open_to_public) {
-        data = data.filter(dest => dest.is_open_to_public === JSON.parse(is_open_to_public)
-        )
+        data = data.filter(dest => dest.is_open_to_public === JSON.parse(is_open_to_public.toLowerCase()))
     }
+
     return data
 
 }
