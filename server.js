d29aa975dfd7984d1e0f8c3186337d394c02084a
sujan2000 <meena.sujan06@gmail.com>
2026-01-06 20:40:09 +0530

    server file update and refactored

--- server.js
+++ server.js
@@ -1,29 +1,39 @@
 import http from 'node:http';
 import { getDataFromDB } from './database/db.js';
 import { sendJSONReasponse } from './utils/sendJSONReasponse.js'
+import { getDataByPathParams } from './utils/getDataByPathParams.js'
+import { getDataByQueryParams } from './utils/getDataByQueryParams.js'
+
+
 
 const PORT = 8000;
 const server = http.createServer(async (req, res) => {
 
+    const urlObj = new URL(req.url, `http://${req.headers.host}`)
+    const queryObj = Object.fromEntries(urlObj.searchParams)
+
     const destinations = await getDataFromDB()
 
-    if (req.url === '/api' && req.method === 'GET') {
-
-        sendJSONReasponse(res, 200, destinations)
+    if (urlObj.pathname === '/api' && req.method === 'GET') {
+        const filteredData = destinations
+        console.log(queryObj)
+        sendJSONReasponse(res, 200, filteredData)
 
     } else if (req.url.startsWith('/api/continents') && req.method === 'GET') {
 
         const continent = req.url.split('/').pop()
-        console.log(continent)
-        const filterDestinations = destinations.filter(dest => {
-            return dest.continent.toLowerCase() === continent.toLowerCase()
-        })
+        const filterContinent = getDataByPathParams(destinations, 'continent', continent)
+        sendJSONReasponse(res, 200, filterContinent)
 
+    } else if (req.url.startsWith('/api/countries') && req.method === 'GET') {
+
+        const country = req.url.split('/').pop()
+        const filterDestinations = getDataByPathParams(destinations, 'country', country)
         sendJSONReasponse(res, 200, filterDestinations)
 
-    }
-    else {
-        sendJSONReasponse(res, 404, { error: "not found", message: "The requested route does not exists" })
+    } else {
+        const message = { error: "not found", message: "The requested route does not exists" }
+        sendJSONReasponse(res, 404, (message))
     }
 })
 
